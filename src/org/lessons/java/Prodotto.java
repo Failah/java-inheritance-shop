8b3bcdbe26cb3e8f160aa961e7dd12524131cfd3
ValerioScharizza <valerio.schiarizza@live.it>
2023-01-30 14:08:15 +0100

    start exercise

---
+++ src/org/lessons/java/Prodotto.java
@@ -0,0 +1,58 @@
+/*
+ * Creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- marca
+- prezzo
+- iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri 
+metodi di “utilità” per fare in modo che:
+- il codice prodotto sia accessibile solo in lettura
+- gli altri attributi siano accessibili sia in lettura che in scrittura
+- il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva formattato
+Lo shop gestisce diversi tipi di prodotto:
+- Smartphone, caratterizzati anche dal codice IMEI e dalla quantità di memoria
+- Televisori, caratterizzati dalle dimensioni e dalla proprietà di essere smart oppure no
+- Cuffie, caratterizzate dal colore e se sono wireless o cablate
+Utilizzate l’ereditarietà per riutilizzare il codice di Prodotto nella stesura delle classi che gestiscono i vari sotto tipi di prodotto.
+BONUS: create una classe Catalogo con metodo main, in cui chiedete all’utente di valorizzare un catalogo di prodotti con dati 
+inseriti tramite scanner.
+Durante la richiesta di valorizzazione chiedete all’utente se sta inserendo uno Smartphone o un Televisore o Cuffie e in base alla 
+scelta dell’utente utilizzate il costruttore opportuno.
+Al termine dell’inserimento stampate il catalogo (fate l’override del metodo toString per restituire le informazioni da stampare 
+per ogni classe)
+ */
+
+package org.lessons.java;
+
+import java.util.Random;
+
+public class Prodotto {
+	String code;
+	String name;
+	String description;
+	double price;
+	double IVA = 22;
+
+	public Prodotto(String name, Double price) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.price = price;
+
+		Random r = new Random();
+		this.code = String.format("%08d", r.nextInt(9998) + 1);
+	}
+
+	double getPrice() {
+		return this.price;
+	}
+
+	double getPriceIVA() {
+		return (price + (price * 22) / 100);
+	}
+
+	String getExtendedName() {
+		return this.code + this.name;
+	}
+}
